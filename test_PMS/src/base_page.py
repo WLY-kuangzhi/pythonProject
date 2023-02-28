430531b868b79c9a0a7ca7032bdab84244047336
WLY-kz <15779286347@163.com>
2023-02-28 11:26:25 +0800

    1234

---
+++ test_PMS/src/base_page.py
@@ -0,0 +1,31 @@
+import json
+
+import yaml
+
+
+class BasePage:
+    # 存放公共变量
+    def __init__(self):
+        self.cookies = {
+            "fat.auth.token": "4F21F266508EA95427E5FEA04E70EE6E"
+        }
+        self.header = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+    # 获取yaml文件内容
+    def get_yaml_data(self):
+        try:
+            with open('../pms_data.yml', encoding="utf-8") as f:
+                data = yaml.load(f, Loader=yaml.FullLoader)
+                return data
+        except:
+            return None
+
+    # 修改yaml文件值
+    def update_yaml_data(self, r, v):
+        old_data = self.get_yaml_data()
+        old_data[0][r] = v
+        with open('../pms_data.yml', "w", encoding="utf-8") as f:
+            yaml.dump(old_data, f, allow_unicode=True)
+
